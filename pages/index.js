9ab2466321df838c2d351e1792e95579c3877e3d
higashi <higashi@siu.co.jp>
2021-09-10 17:52:47 +0900

    コンポーネントの追加

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,12 @@ import Image from 'next/image'
 import styles from '../styles/index.module.css'
 import Link from 'next/link'
 
+import Header from './components/header'
+
 export default function Home() {
   return (
     <div>
+      <Header title="へっだーたいとる"></Header>
       <h1 style={{backgroundColor : `red`}}>たいとる</h1>
       <p>学ぶ。ちゃんと</p>
       <div className={styles.mytitle}>Title</div>
