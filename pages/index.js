090d758a1edec7195159e65b265011e292eec19f
higashi <higashi@siu.co.jp>
2021-09-10 17:40:40 +0900

    外部スタイルシートの記述と使用方法について

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/index.module.css'
 
 export default function Home() {
   return (
     <div>
       <h1 style={{backgroundColor : `red`}}>たいとる</h1>
       <p>学ぶ。ちゃんと</p>
+      <div className={styles.mytitle}>Title</div>
     </div>
     
   )
