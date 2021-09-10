e670609410f22037e01741b31f487dabcd9b32c2
higashi <higashi@siu.co.jp>
2021-09-10 17:33:43 +0900

    StyleでCSSにハイフンを含む場合はキャメルで書く必要がある

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      <h1 className="mytitle" style={{ color : `red`}}>たいとる</h1>
+      <h1 style={{backgroundColor : `red`}}>たいとる</h1>
       <p>学ぶ。ちゃんと</p>
     </div>
     
